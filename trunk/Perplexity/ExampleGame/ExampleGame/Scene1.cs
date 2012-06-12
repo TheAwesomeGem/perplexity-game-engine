260466b8179d378c93b887b7f54582cc1586aeb2
mehtabtowheed@yahoo.com <mehtabtowheed@yahoo.com@ee403d6f-cd19-8fa2-03f1-c555f3269559>
2012-06-12 19:30:37 +0000

    Improved Example Game.

--- trunk/Perplexity/ExampleGame/ExampleGame/Scene1.cs
+++ trunk/Perplexity/ExampleGame/ExampleGame/Scene1.cs
@@ -11,13 +11,25 @@ namespace ExampleGame
     {
         public Scene1(Game game)
         {
-            Actor guy = new Actor(this, game.Content.Load<Texture2D>("player"), new Vector2(300, 300), 0);
+            Actor guy = new Actor(this, game.Content.Load<Texture2D>("player"), new Vector2(10, 0), 1);
             EntityManager.AddEntity("Guy", guy);
+            Show();
         }
 
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
+
+            Actor guy = EntityManager.GetEntity<Actor>("Guy");
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                guy.Translate(new Vector2(-2f, 0));
+
+            if (guy.Position.X < 0)
+            {
+                this.Hide();
+                SceneManager.GetScene("Scene2").Show();
+            }
         }
         public override void Draw(SpriteBatch spriteBatch)
         {
